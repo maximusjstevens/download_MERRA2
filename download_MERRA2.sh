1503c0dee45087958444ec5724e3e4a6389e6630
Eric Keenan <eric.keenan@colorado.edu>
2019-08-20 15:47:03 -0600

    Updated user editable variables

--- download_MERRA2.sh
+++ download_MERRA2.sh
@@ -5,11 +5,29 @@
 # $ bash download_MERRA2.sh
 
 # To Do
-# 1. Add blurb about creating a NASA login
-# 2. Update script for user editable variables. 
+# 1. Update script for user editable variables. 
+
+##### Begin User Editable Variables #####
+
+year_start="1980"
+year_end="1980"
+
+month_start="1"
+month_end="12"
+
+day_start="1"
+day_end="31"
+
+lat_min="-90"
+lat_max="-40"
+lon_min="-180"
+lon_max="180"
+
+##### End User Editable Variables #####
 
 # First print wget commands to files
-python3 print_wget_commands.py
+python3 print_wget_commands.py ${year_start} ${year_end} ${month_start} ${month_end} \
+	${day_start} ${day_end} ${lat_min} ${lat_max} ${lon_min} ${lon_max}
 
 # Now run wget to retrieve data
 bash retrieve_data.sh
--- print_wget_commands.py
+++ print_wget_commands.py
@@ -4,18 +4,26 @@
 import pandas as pd
 import datetime
 import os
+import sys
 
-# Define user editable variables
-year_start = 1980
-year_end = 1980
+# Retrieve user editable variables
+year_start = sys.argv[1]
+year_end = sys.argv[2]
 
-lat_min = "-90"
-lat_max = "-40"
-lon_min = "-180"
-lon_max = "180"
+month_start = sys.argv[3]
+month_end = sys.argv[4]
+
+day_start = sys.argv[5]
+day_end = sys.argv[6]
+
+lat_min = sys.argv[7]
+lat_max = sys.argv[8]
+lon_min = sys.argv[9]
+lon_max = sys.argv[10]
 
 # Create date object
-date = pd.date_range(start='1/1/' + str(year_start), end='12/31/' + str(year_end))
+date = pd.date_range(start = str(month_start) + "/" + str(day_start) + "/" + str(year_start), \
+	end =str(month_end) + "/" + str(day_end) + "/" + str(year_end))
 
 # Define file names and delete old versions
 surface_wget_url_file = "./surface/wget_surface.sh"
@@ -38,7 +46,7 @@ for day in range(0, 3):
 	surface_str = "http://goldsmr4.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?FILENAME=" \
 		+ "%2Fdata%2FMERRA2%2FM2T1NXSLV.5.12.4%2F" + str(day_year) + "%2F01%2FMERRA2_100.tavg1_2d_slv_Nx." \
 		+ day_str \
-		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + lat_min + "%2C" + lon_min + "%2C" + lat_max + "%2C" + lon_max \
+		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + str(lat_min) + "%2C" + str(lon_min) + "%2C" + str(lat_max) + "%2C" + str(lon_max) \
 		+ "&LABEL=MERRA2_100.tavg1_2d_slv_Nx.19800101.SUB.nc4&SHORTNAME=M2T1NXSLV&SERVICE=SUBSET_ME" \
 		+ "RRA2&VERSION=1.02&LAYERS=&VARIABLES=ps%2Cqv2m%2Ct2m%2Cts%2Cu10m%2Cv10m\n"
 
@@ -51,7 +59,7 @@ for day in range(0, 3):
 	precip_str = "http://goldsmr4.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?FILENAME=" \
 		+ "%2Fdata%2FMERRA2%2FM2T1NXINT.5.12.4%2F" + str(day_year) + "%2F01%2FMERRA2_100.tavg1_2d_int_Nx." \
 		+ day_str \
-		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + lat_min + "%2C" + lon_min + "%2C" + lat_max + "%2C" + lon_max \
+		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + str(lat_min) + "%2C" + str(lon_min) + "%2C" + str(lat_max) + "%2C" + str(lon_max) \
 		+ "&LABEL=MERRA2_100.tavg1_2d_int_Nx.19800101.SUB.nc4&SHORTNAME=M2T1NXINT&SERVICE=SUBSET_ME" \
 		+ "RRA2&VERSION=1.02&LAYERS=&VARIABLES=preccu%2Cprecls%2Cprecsn\n"
 
@@ -64,7 +72,7 @@ for day in range(0, 3):
 	radiation_str = "http://goldsmr4.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?FILENAME=" \
 		+ "%2Fdata%2FMERRA2%2FM2T1NXRAD.5.12.4%2F" + str(day_year) + "%2F01%2FMERRA2_100.tavg1_2d_rad_Nx." \
 		+ day_str \
-		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + lat_min + "%2C" + lon_min + "%2C" + lat_max + "%2C" + lon_max \
+		+ ".nc4&FORMAT=bmM0Lw&BBOX=" + str(lat_min) + "%2C" + str(lon_min) + "%2C" + str(lat_max) + "%2C" + str(lon_max) \
 		+ "&LABEL=MERRA2_100.tavg1_2d_rad_Nx.19800101.SUB.nc4&SHORTNAME=M2T1NXRAD&SERVICE=SUBSET_ME" \
 		+ "RRA2&VERSION=1.02&LAYERS=&VARIABLES=lwgab%2Clwgem%2Clwgnt%2Cswgdn%2Cswgnt\n"
 
