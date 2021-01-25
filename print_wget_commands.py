4ba1ccc459199be7be85c5115fdb164c90b2ca84
EricKeenan <eric.keenan@colorado.edu>
2021-01-25 09:58:04 -0700

    Update stream to automatically retrieve latest data.

--- print_wget_commands.py
+++ print_wget_commands.py
@@ -51,35 +51,37 @@ for day in range(0, len(date)):
 
 	stream = "-999"
 	if day_year >= 1980 and  day_year <= 1990:
-		stream = "1"
+		stream = "100"
 	elif day_year > 1990 and  day_year <= 2000:
-		stream = "2"
+		stream = "200"
 	elif day_year > 2000 and  day_year <= 2010:
-		stream = "3"
+		stream = "300"
+	elif day_year == 2020 and day_month == "09":
+		stream = "401"
 	else:
-		stream = "4"
+		stream = "400"
 
 	##### Surface #####
 	surface_file = open(surface_wget_url_file, "a")
-	surface_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + "00.tavg1_2d_slv_Nx." + str(day_str) + ".nc4.nc4?H500[0:23][0:100][0:575],PS[0:23][0:100][0:575],QV2M[0:23][0:100][0:575],T2M[0:23][0:100][0:575],TS[0:23][0:100][0:575],U10M[0:23][0:100][0:575],V10M[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
+	surface_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + ".tavg1_2d_slv_Nx." + str(day_str) + ".nc4.nc4?H500[0:23][0:100][0:575],PS[0:23][0:100][0:575],QV2M[0:23][0:100][0:575],T2M[0:23][0:100][0:575],TS[0:23][0:100][0:575],U10M[0:23][0:100][0:575],V10M[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
 	surface_file.write(surface_str)
 	surface_file.close()
 
 	##### Precipitation #####
 	precip_file = open(precip_wget_url_file, "a")
-	precip_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXINT.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + "00.tavg1_2d_int_Nx." + str(day_str) + ".nc4.nc4?PRECCU[0:23][0:100][0:575],PRECLS[0:23][0:100][0:575],PRECSN[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
+	precip_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXINT.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + ".tavg1_2d_int_Nx." + str(day_str) + ".nc4.nc4?PRECCU[0:23][0:100][0:575],PRECLS[0:23][0:100][0:575],PRECSN[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
 	precip_file.write(precip_str)
 	precip_file.close()
 
 	##### Radiation #####
 	radiation_file = open(radiation_wget_url_file, "a")
-	radiation_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + "00.tavg1_2d_rad_Nx." + str(day_str) + ".nc4.nc4?LWGAB[0:23][0:100][0:575],LWGEM[0:23][0:100][0:575],LWGNT[0:23][0:100][0:575],SWGDN[0:23][0:100][0:575],SWGNT[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
+	radiation_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXRAD.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + ".tavg1_2d_rad_Nx." + str(day_str) + ".nc4.nc4?LWGAB[0:23][0:100][0:575],LWGEM[0:23][0:100][0:575],LWGNT[0:23][0:100][0:575],SWGDN[0:23][0:100][0:575],SWGNT[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
 	radiation_file.write(radiation_str)
 	radiation_file.close()
 
 	##### Evaporation #####
 	evap_file = open(evap_wget_url_file, "a")
-	evap_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + "00.tavg1_2d_flx_Nx." + str(day_str) + ".nc4.nc4?EVAP[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
+	evap_str = "https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXFLX.5.12.4/" + str(day_year) + "/" + str(day_month) + "/MERRA2_" + str(stream) + ".tavg1_2d_flx_Nx." + str(day_str) + ".nc4.nc4?EVAP[0:23][0:100][0:575],time,lat[0:100],lon[0:575]\n"
 	evap_file.write(evap_str)
 	evap_file.close()
 
