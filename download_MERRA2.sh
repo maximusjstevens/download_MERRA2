5cbe6d56d71b22ad7d6000754709d3c246818414
Max Stevens <maximusjstevens@gmail.com>
2022-04-21 12:30:00 -0700

    create_forcing_nc.sh:  - added datapath as variable, allows files to be anywhere and not hardcoded  - added rm at end to get rid of tmp files  - commented out prefix lines for general use  - changed cdo calls to exclude hard-coded path to cdo
    download_MERRA2.sh:
     - added datapath as variable that gets passed to subscripts (retrieve_data.sh, create_forcing_nc.sh)
    
    retrieve_data.sh:
     - datapath is now passed in as a variable instead of being coded in this script.
    
    general:
     - edited time-span variables to that wget_*.sh scripts are only a few lines long for example sake.

--- download_MERRA2.sh
+++ download_MERRA2.sh
@@ -16,20 +16,23 @@
 
 ##### Begin User Editable Variables #####
 
-year_start="1980"
-year_end="2021"
+year_start="2020"
+year_end="2020"
 
 month_start="1"
-month_end="12"
+month_end="1"
 
 day_start="1"
-day_end="31"
+day_end="3"
 
 # Set the bounding box for spatial extent
 lat_min="55"
 lat_max="90"
 lon_min="-80"
 lon_max="-10"
+
+datapath=$PWD #If you want to download and save the files to the current directory (and subdirectories)
+# datapath="/PATH/TO/DIRECTORY" # Put the downloaded files somewhere else.
 
 ##### End User Editable Variables #####
 
@@ -38,9 +41,9 @@ python3 print_wget_commands.py ${year_start} ${year_end} ${month_start} ${month_
 	${day_start} ${day_end} ${lat_min} ${lat_max} ${lon_min} ${lon_max}
 
 # Now run wget to retrieve data
-bash retrieve_data.sh
+bash retrieve_data.sh $datapath
 
-# Process files into .nc
-# mkdir -p nc_files
-# rm nc_files/*.nc
-# csh create_forcing_nc.sh $year_start $year_end
+# Process files into individual .nc files for each variable, for each year
+mkdir -p "$datapath/nc_files"
+rm "$datapath/nc_files/*.nc"
+csh create_forcing_nc.sh $year_start $year_end $datapath
--- retrieve_data.sh
+++ retrieve_data.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-# Option to specify where to save the MERRA-2 files (MS)
-dpth='/Volumes/Samsung_T1/MTEST' #save in present directory
-# dpth='/PATH/TO/DIRECTORY' #e.g. if you want to put on an externa drive
+datapath=$1 #e.g. if you want to put on an externa drive
 
-# This script will retrieve data by executing wget URLs. 
-# cd surface
-# rm MERRA2*
-# rm tmp*
-# wget -P $dpth --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_surface.sh
-# cd ..
+### This script will retrieve data by executing wget URLs. 
+cd surface
+rm MERRA2*
+rm tmp*
+wget -P "$datapath/surface" --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_surface.sh
+cd ..
 
 cd radiation
 rm MERRA2*
 rm tmp*
-wget -P $dpth --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_radiation.sh
+wget -P "$datapath/radiation" --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_radiation.sh
 cd ..
 
-# cd precip
-# rm MERRA2*
-# rm tmp*
-# wget -P $dpth --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_precip.sh
-# cd ..
+cd precip
+rm MERRA2*
+rm tmp*
+wget -P "$datapath/precip" --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_precip.sh
+cd ..
 
-# cd evap
-# rm MERRA2*
-# rm tmp*
-# wget -P $dpth --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_evap.sh
-# cd ..
+cd evap
+rm MERRA2*
+rm tmp*
+wget -P "$datapath/evap" --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i wget_evap.sh
+cd ..
